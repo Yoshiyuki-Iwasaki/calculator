2a8005690840d3f7f7518ca54816b6eee5b19a83
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-20 18:43:40 +0900

    create redux reducer file.

---
+++ src/redux/reducers/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { combineReducers } from "redux";
+import Calculator from "../../components/organisms/calculator";
+
+const Reducer = combineReducers({ Calculator });
+
+export default Reducer;
