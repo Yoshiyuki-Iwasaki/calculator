96ba13dea795330c13fd767bdc072e916ba3e33f
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-20 18:43:30 +0900

    create redux actions file.

---
+++ src/redux/actions/index.tsx
@@ -0,0 +1,33 @@
+import {
+  INPUT_NUMBER,
+  PLUS,
+  MINUS,
+  MULTIPLY,
+  DIVIDE,
+  EQUAL,
+  CLEAR,
+} from "./type";
+
+export const onNumberClick = (number: number) => ({
+  type: INPUT_NUMBER,
+  number,
+});
+
+export const onPlusClick = () => ({
+  type: PLUS,
+});
+export const onMinusClick = () => ({
+  type: MINUS,
+});
+export const onMultiplyClick = () => ({
+  type: MULTIPLY,
+});
+export const onDivideClick = () => ({
+  type: DIVIDE,
+});
+export const onEqualClick = () => ({
+  type: EQUAL,
+});
+export const onClearClick = () => ({
+  type: CLEAR,
+});
